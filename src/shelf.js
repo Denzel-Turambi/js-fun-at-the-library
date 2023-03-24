5864e829471351ec1194f4885d0461f895d81e16
Denzel Turambi <dturambi26@gmail.com>
2023-03-24 17:58:08 -0600

    Add unshelfBook function

--- src/shelf.js
+++ src/shelf.js
@@ -1,14 +1,22 @@
-function shelfBook(book, shelf) {
+function shelfBook(bookTitle, shelf) {
   if (shelf.length < 3) {
-    shelf.unshift(book);
+    shelf.unshift(bookTitle);
     return shelf
   }
 }
 
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === bookTitle) {
+      shelf.splice(i, 1);
+      return shelf;
+    }
+  }
+};
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
