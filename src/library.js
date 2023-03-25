59e9f0fc14af38106f95e646127986f31ee8c6e8
Denzel Turambi <dturambi26@gmail.com>
2023-03-25 14:32:29 -0600

    Add addBooks function

--- src/library.js
+++ src/library.js
@@ -1,4 +1,4 @@
-function createLibrary(name, shelves) {
+function createLibrary(name) {
   libraryInfo = {
     name,
     shelves: {
@@ -10,9 +10,22 @@ function createLibrary(name, shelves) {
   return libraryInfo;
 }
 
+function addBook(library, book) {
+  if (book.genre === "fantasy") {
+    libraryInfo.shelves.fantasy.push(book);
+    return library;
+  } else if (book.genre === "fiction") {
+    libraryInfo.shelves.fiction.push(book);
+    return library;
+  } else if (book.genre === "nonFiction") {
+    libraryInfo.shelves.nonFiction.push(book);
+    return library;
+  }
+}
+
 module.exports = {
   createLibrary,
-  // addBook,
+  addBook,
   // checkoutBook,
   // takeStock
 };
