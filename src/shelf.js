4d2600f669f4b64fa000a9857dd1685590e92b3a
Denzel Turambi <dturambi26@gmail.com>
2023-03-24 19:59:53 -0600

    Add listTitles function

--- src/shelf.js
+++ src/shelf.js
@@ -1,22 +1,32 @@
-function shelfBook(bookTitle, shelf) {
+function shelfBook(book, shelf) {
   if (shelf.length < 3) {
-    shelf.unshift(bookTitle);
+    shelf.unshift(book);
     return shelf
   }
 }
 
-function unshelfBook(bookTitle, shelf) {
+function unshelfBook(book, shelf) {
   for (var i = 0; i < shelf.length; i++) {
-    if(shelf[i].title === bookTitle) {
+    if(shelf[i].title === book) {
       shelf.splice(i, 1);
       return shelf;
     }
   }
 };
 
+function listTitles( shelf) {
+  // take titles from each object, and put them into a single string data type
+  titles = []
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+    console.log(titles);
+  }
+  return titles.join(", ");
+}
+
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
+  listTitles,
   // searchShelf
 };
