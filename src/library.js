d0eb6f0ed31bc610664b7b1ca12e0a5df9c21118
Denzel Turambi <dturambi26@gmail.com>
2023-03-26 15:05:10 -0600

    Refactor addBook function, and add checkoutBook function

--- src/library.js
+++ src/library.js
@@ -11,21 +11,42 @@ function createLibrary(name) {
 }
 
 function addBook(library, book) {
-  if (book.genre === "fantasy") {
-    libraryInfo.shelves.fantasy.push(book);
-    return library;
-  } else if (book.genre === "fiction") {
-    libraryInfo.shelves.fiction.push(book);
-    return library;
-  } else if (book.genre === "nonFiction") {
-    libraryInfo.shelves.nonFiction.push(book);
-    return library;
-  }
+  // if (book.genre === "fantasy") {
+  //   libraryInfo.shelves.fantasy.push(book);
+  //   return library;
+  // } else if (book.genre === "fiction") {
+  //   libraryInfo.shelves.fiction.push(book);
+  //   return library;
+  // } else if (book.genre === "nonFiction") {
+  //   libraryInfo.shelves.nonFiction.push(book);
+  //   return library;
+  // }
+  // This is the above code but refactored to become more dry
+  var bookGenre = book.genre;
+  library.shelves[bookGenre].push(book);
+  return library;
 }
+
+function checkoutBook(library, bookTitle, bookGenre) {
+  var bookExists = false;
+  for(var i = 0; i < library.shelves[bookGenre].length; i++) { 
+    if(library.shelves[bookGenre][i].title === bookTitle) {
+      library.shelves[bookGenre].splice(i, 1); 
+      bookExists = true;
+    }
+  } 
+if(bookExists === true) {
+  return `You have now checked out ${bookTitle} from the ${library.name}.`; 
+ } else {
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}.`
+ }
+
+}
+
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook,
+  checkoutBook,
   // takeStock
 };
